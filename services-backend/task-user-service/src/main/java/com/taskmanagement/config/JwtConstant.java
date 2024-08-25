0af8aac2b593c4e829546ab2ecd548a63163cdfe
unknown <jaime.jgsn2@gmail.com>
2024-08-25 13:43:29 -0300

    User Service

---
+++ services-backend/task-user-service/src/main/java/com/taskmanagement/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.taskmanagement.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="adbahjdbashjdbqwhjdbqwhjdbajkjdnjak";
+    public static final String JWT_HEADER="Authorization";
+}
---
+++ task-service/src/main/java/com/taskservice/controller/TaskController.java
@@ -0,0 +1,2 @@
+package com.taskservice.controller;public class TaskController {
+}
